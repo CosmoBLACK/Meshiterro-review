6897fb676da988b0cff39720aaeeab7ee8d071ea
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-07-30 13:55:56 +0000

    [Add] ActiveStorage、image_processing導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "f72e21a53fad4c158fa22f2b5080129e.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
