dd0500abe40e76fc8198147e9105053e1bb6af8b
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-03 12:03:52 +0000

    [add] post_imageモデルにget_imageメソッド作成

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,4 +2,13 @@ class PostImage < ApplicationRecord
   # ActiveStoreage を使って画像を持たせる
   has_one_attached :image
   belongs_to :user
+  
+  # メソッド作成
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 end
