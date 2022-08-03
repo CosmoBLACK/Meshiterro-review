8509045e9b20255c4151a7c87af4ce96845dc13e
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-03 11:23:12 +0000

    [Add] PostImagesコントローラ追加、ルーティング生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  # resourcesメソッドは、ルーティングを一括して自動生成してくれる機能を有する
+  # onlyオプションを使用することで、生成するルーティングを限定する
+  resources :post_images, only: [:new, :index, :show]
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
