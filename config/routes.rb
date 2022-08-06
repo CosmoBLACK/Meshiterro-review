f5dd761cb8c5ef1deb08ea21780f9af7cfb6bf93
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-06 09:54:28 +0000

    [Add] Usesコントローラ作成、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   # resourcesメソッドは、ルーティングを一括して自動生成してくれる機能を有する
   # onlyオプションを使用することで、生成するルーティングを限定する
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
   get 'homes/about' => 'homes#about', as: 'about'
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
