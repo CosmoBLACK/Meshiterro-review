5625cef6c88c5a6774ea9445a37c5988df6dd217
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-06 10:07:51 +0000

    [Add] ユーザーの詳細画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # 特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得し@post_imagesに渡す
+    @post_images = @user.post_images
+  end
+  
+  def edit
+  end
 end
