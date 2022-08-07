69866ec78a33106c465955e1c8ed995f8297ca45
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-07 15:10:02 +0000

    [Add] 更新機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,20 @@ class UsersController < ApplicationController
     # 特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得し@post_imagesに渡す
     @post_images = @user.post_images
   end
-  
+
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
