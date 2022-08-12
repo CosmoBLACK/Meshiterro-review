aa8eda17274c7a080410fbb0e4926f0095b8fae2
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-08-12 12:35:46 +0000

    [Update] 権限の設定（ログインしていなければ、TOP画面以外の画面（ログインと新規登録は除く）は表示できない）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  # before_actionメソッドは、このコントローラが動作する前に実行される
+  # authenticate_userメソッドは、Devise側が用意しているメソッドで、:authenticate_user!とすることによって、「ログイン認証されていなければ、ログイン画面へリダイレクトする」機能を実装できる
+  # exceptは指定したアクションをbefore_actionの対象から外す機能があり、トップページのみログイン状態に関わらず、アクセス可能とするためにtopアクションを指定している
+  before_action :authenticate_user!, except: [:top]
   # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される
   before_action :configure_permitted_parameters, if: :devise_controller?
   # deviseが用意しているメソッドで、サインイン後にどこに遷移するかを設定するメソッド
